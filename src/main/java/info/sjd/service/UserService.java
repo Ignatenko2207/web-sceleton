27ded5f5ab258de6272a04d31d31bc6c752732f3
Alex <ignatenko2207@gmail.com>
2019-07-26 20:26:51 +0300

    Add mock to UserServiceTest

--- src/main/java/info/sjd/service/UserService.java
+++ src/main/java/info/sjd/service/UserService.java
@@ -1,15 +1,22 @@
 package info.sjd.service;
 
+import info.sjd.dao.UserDAO;
 import info.sjd.model.User;
 
 public class UserService {
 
-    public static User findUserByLoginAndPassword(String login, String password){
-        User user = new User("Alex", "Ignatenko", "ignatenko2207", "123456");
-
-        if(user.getLogin().equals(login) && user.getPassword().equals(password)){
-            return user;
+    public static User create(User user){
+        if (UserDAO.findByLogin(user.getLogin()) == null){
+            return UserDAO.create(user);
         }
         return null;
+    }
+
+    public static User findByLogin(String login){
+        return UserDAO.findByLogin(login);
+    }
+
+    public static User findUserByLoginAndPassword(String login, String password){
+        return UserDAO.findByLoginAndPassword(login, password);
     }
 }
